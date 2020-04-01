c9b3b0cc16c7a110c90c92c9de3ea7d0ea1a470d
Jean Sarlôn <jean.sarlon@icloud.com>
2020-03-31 21:44:05 -0300

    test: change test to get element by class

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('should renders component', () => {
+    const { container } = render(<App />);
+    const element = container.querySelector('.App');
+    expect(element).toBeInTheDocument();
+  });
 });
+
