d00715178a02d2a668a30086c582c927532dafa8
Jean Sarlôn <jean.sarlon@icloud.com>
2020-04-06 08:39:52 -0300

    style: change place of setupTest file

--- src/setupTests.ts
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-import DotEnv from 'dotenv';
-
-DotEnv.config({ path: './configuration/envs/.env.test' });
-global.ENVIRONMENT = process.env.NODE_ENV;
-
