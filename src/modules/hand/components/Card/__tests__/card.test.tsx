475f427e92fbafc0d32e2175f14f7a71fc3299b1
Jean Sarlôn <jean.sarlon@icloud.com>
2020-04-06 03:47:26 -0300

    test: refactor test to card component

--- src/modules/hand/components/Card/__tests__/card.test.tsx
+++ src/modules/hand/components/Card/__tests__/card.test.tsx
@@ -1,14 +1,50 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
+import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
+import { getCards } from '../../../../../services/pokemonTCG';
 import Card from '..';
 
-describe('Card', () => {
-  test('should mount Component', () => {
-    const { container } = render(<Card />);
+jest.mock('../../../../../services/pokemonTCG');
 
-    const element = container.querySelector('.card');
+describe('Card', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('should mount Component when has cards', async () => {
+    const mockedUseFooContext = getCards as jest.Mock<Promise<Partial<PokemonTCG.Card>[]>>;
+    mockedUseFooContext.mockImplementation(() => {
+      return new Promise((resolve) => resolve([{ id: '1', name: 'pikachu' }]));
+    });
+    let component,
+      element;
+
+    await act(async () => {
+      const { container } = await render(<Card />);
+      component = container;
+      element = container.querySelector('.card');
+    });
+
+    expect(component).toMatchSnapshot();
     expect(element).toBeDefined();
-    expect(container).toMatchSnapshot();
+  });
+
+  test('should mount Component when has no cards', async () => {
+    const mockedUseFooContext = getCards as jest.Mock<Promise<PokemonTCG.Card[] | undefined>>;
+    mockedUseFooContext.mockImplementation(() => {
+      return new Promise((resolve) => resolve(undefined));
+    });
+    let component;
+
+    await act(async () => {
+      const { container } = await render(<Card />);
+      component = container;
+    });
+
+    expect(component).toMatchSnapshot();
   });
 });
 
