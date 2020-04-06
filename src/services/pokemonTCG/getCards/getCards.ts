0e1696e963876107ba7c810cbabaa4e21235621f
Jean Sarlôn <jean.sarlon@icloud.com>
2020-04-06 08:38:11 -0300

    refactor: add method to get cards from stubby

--- src/services/pokemonTCG/getCards/getCards.ts
+++ src/services/pokemonTCG/getCards/getCards.ts
@@ -1,5 +1,17 @@
 import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
 import { IcardFilter } from './CardFiltersBuilder';
 
-export const getCards = async (filters: IcardFilter[]): Promise<PokemonTCG.Card[] | undefined> =>
-  await PokemonTCG.Card.where(filters);
+const getCardsLocal = () => {
+  const { REACT_APP_POKEMON_TCG_HOST, REACT_APP_POKEMON_TCG_CARDS_PATH } = process.env;
+  return fetch(`${REACT_APP_POKEMON_TCG_HOST}${REACT_APP_POKEMON_TCG_CARDS_PATH}`)
+    .then((res) => res.json())
+    .then((data) => data.cards);
+};
+
+export const getCards = async (filters: IcardFilter[]): Promise<PokemonTCG.Card[] | undefined> => {
+  switch (process.env.NODE_ENV) {
+    case 'development': return await getCardsLocal();
+    default: return await PokemonTCG.Card.where(filters);
+  }
+};
+
