c410184ba89573dd5b4b3762f5924d5c9c3d756f
Jean Sarlôn <jean.sarlon@icloud.com>
2020-04-14 13:03:47 -0300

    refactor: hand now call and render cards

--- src/modules/hand/components/Hand/__tests__/hand.test.tsx
+++ src/modules/hand/components/Hand/__tests__/hand.test.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
+import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
+import { getCards } from '../../../../../services/pokemonTCG';
 import Hand from '..';
 
-describe('Hand', () => {
-  test('should mount Component', () => {
-    const { container } = render(<Hand />);
+jest.mock('../../../../../services/pokemonTCG');
 
-    const element = container.querySelector('.hand');
-    expect(element).toBeInTheDocument();
-    expect(container).toMatchSnapshot();
+describe('Hand', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterAll(() => {
+    jest.restoreAllMocks();
+  });
+
+  test('should mount Component with cards', async () => {
+    const mockedUseFooContext = getCards as jest.Mock<Promise<Partial<PokemonTCG.Card>[]>>;
+    mockedUseFooContext.mockImplementation(() => {
+      return new Promise((resolve) => resolve([{ id: '1', name: 'pikachu' }]));
+    });
+    let component,
+      element;
+    await act(async () => {
+      const { container } = render(<Hand />);
+      element = container.querySelector('.hand');
+      component = container;
+    });
+
+    expect(element).toBeDefined();
+    expect(component).toMatchSnapshot();
+  });
+
+  test('should mount Components without cards', async () => {
+    const mockedUseFooContext = getCards as jest.Mock<Promise<Partial<PokemonTCG.Card>[]>>;
+    mockedUseFooContext.mockImplementation(() => {
+      return new Promise((resolve) => resolve([]));
+    });
+    let component;
+    await act(async () => {
+      const { container } = render(<Hand />);
+      component = container;
+    });
+    expect(component).toMatchSnapshot();
   });
 });
 
--- src/modules/hand/components/Hand/index.tsx
+++ src/modules/hand/components/Hand/index.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ICard } from 'pokemon-tcg-sdk-typescript/dist/sdk';
+import { IcardFilter } from '../../../../services/pokemonTCG/getCards/CardFiltersBuilder';
+import { CardFiltersBuilder } from '../../../../services/pokemonTCG/getCards';
+import { getCards } from '../../../../services/pokemonTCG';
 import { Card } from '..';
 
-const Hand = () => (
-  <div className="hand">
-    hand
-    <Card />
-  </div>
-);
+const getcards = async (callback: Function) => {
+  const data: IcardFilter[] = new CardFiltersBuilder()
+    .filter({ name: 'supertype', value: 'pokemon' })
+    .build();
+
+  const response = await getCards(data);
+  callback(response);
+};
+
+const Hand = () => {
+  const [cards, setcards] = useState<ICard[] | undefined>([]);
+
+  useEffect(() => {
+    getcards(setcards);
+  }, []);
+
+  return (
+    <div className="hand">
+      <div className="cards">
+        {
+          cards && cards.length
+            ? cards.map((card) => (<Card {...card} key={card.id} />))
+            : <span className="no-cards">Mão vazia</span>
+        }
+      </div>
+    </div>
+  );
+};
 export default Hand;
