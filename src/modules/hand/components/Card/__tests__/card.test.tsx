2267bb5e1f0d91b2dafe9b5d37f0ca13296301b9
Jean Sarlôn <jean.sarlon@icloud.com>
2020-04-14 18:12:14 -0300

    test: refactor card component test

--- src/modules/hand/components/Card/__tests__/card.test.tsx
+++ src/modules/hand/components/Card/__tests__/card.test.tsx
@@ -4,47 +4,23 @@ import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
 import { getCards } from '../../../../../services/pokemonTCG';
 import Card from '..';
 
-jest.mock('../../../../../services/pokemonTCG');
-
 describe('Card', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-  afterAll(() => {
-    jest.restoreAllMocks();
-  });
+  const card = new PokemonTCG.Card();
+  card.name = 'pikachu';
 
-  test('should mount Component when has cards', async () => {
-    const mockedUseFooContext = getCards as jest.Mock<Promise<Partial<PokemonTCG.Card>[]>>;
-    mockedUseFooContext.mockImplementation(() => {
-      return new Promise((resolve) => resolve([{ id: '1', name: 'pikachu' }]));
-    });
-    let component,
-      element;
+  test('should mount Component when has name', async () => {
+    const { container } = render(<Card {...card} />);
+    const element = container.querySelector('.card');
 
-    await act(async () => {
-      const { container } = await render(<Card />);
-      component = container;
-      element = container.querySelector('.card');
-    });
-
-    expect(component).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
     expect(element).toBeDefined();
   });
 
-  test('should mount Component when has no cards', async () => {
-    const mockedUseFooContext = getCards as jest.Mock<Promise<PokemonTCG.Card[] | undefined>>;
-    mockedUseFooContext.mockImplementation(() => {
-      return new Promise((resolve) => resolve(undefined));
-    });
-    let component;
+  test('should mount Component when not has name', async () => {
+    const cardWithoutName = { ...card, name: '' };
+    const { container } = render(<Card {...cardWithoutName} />);
 
-    await act(async () => {
-      const { container } = await render(<Card />);
-      component = container;
-    });
-
-    expect(component).toMatchSnapshot();
+    expect(container).toMatchSnapshot();
   });
 });
 
